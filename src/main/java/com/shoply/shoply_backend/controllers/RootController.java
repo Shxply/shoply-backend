384d936382e98e655df1ec3b7c14b91059668ad1
William Hurley <wdhurleyjr@gmail.com>
2025-03-24 14:14:48 -0700

    Added root controller

---
+++ src/main/java/com/shoply/shoply_backend/controllers/RootController.java
@@ -0,0 +1,14 @@
+package com.shoply.shoply_backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Shoply API is running!";
+    }
+}
+

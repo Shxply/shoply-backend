e6af3dfb64faa311ced3d7d0f8b42953fabb656a
William Hurley <wdhurleyjr@gmail.com>
2025-04-22 13:34:28 -0700

    Updated Google Places API call

--- src/main/java/com/shoply/shoply_backend/utilities/GooglePlacesAPI.java
+++ src/main/java/com/shoply/shoply_backend/utilities/GooglePlacesAPI.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class GooglePlacesAPI {
 
     private static final String API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-    private static final String API_KEY = "AIzaSyDmcL7_-huBbJzehaQfmEeL0MHOSVKBK2g";
+    private static final String API_KEY = System.getenv("GOOGLE_PLACES_API_KEY");
 
     private GooglePlacesAPI() {
         // Prevent instantiation
